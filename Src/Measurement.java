6da43f4505b2fe688cab6cd7d6c25fee0c36f719
EvanHunnik <erwinvanhunnik@hotmail.com>
2019-09-19 12:20:09 +0200

    week 4

--- Src/Measurement.java
+++ Src/Measurement.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Measurement {
+    public static void main(String[] args) {
+       RawMeasurement measurement = DatabaseConnection.getMostRecentMeasurement();
+        System.out.println(measurement);
+        System.out.println(ValueConverter.sunSet(measurement.getSunset()));
+        System.out.println(ValueConverter.airPressure(measurement.getBarometer()));
+        System.out.println(ValueConverter.temperature(measurement.getInsideTemp()));
+
+
+    }
 }
